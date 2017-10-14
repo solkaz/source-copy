c9f1050b37cce792e643fbe011acedc0ccd00dd2
JMH <jheld135@gmail.com>
2017-10-14 11:15:00 -0500

    Add project config files

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js2-mode
+  (prettier-js-args "--trailing-comma=es5" "--single-quote")
+  (prettier-js-mode)))
+
---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.export = {
+  extends: ['eslint:recommended'],
+  env: {
+	  browser: true,
+	  es6: true,
+    webextensions: true,
+  },
+};
