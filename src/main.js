3df11588239263bbcfc2c33fa0a3664998e6e9ca
JMH <jheld135@gmail.com>
2017-10-16 20:07:19 -0500

    Match against the url to apply copy buttons

--- src/main.js
+++ src/main.js
@@ -1,56 +1,80 @@
 import Clipboard from 'clipboard';
+import escapeRegexp from 'lodash-es/escapeRegexp';
 
-// Each code block in Github will be rendered in a div with class="highlight"
-const CODE_BLOCK_SELECTOR = 'div.highlight';
+const httpRegexp = /.*:\/\//;
+const wwwRegexp = /(.*\.)?/;
+const tailRegexp = /.*/;
 
-// Get all code blocks
-const codeBlocks = document.querySelectorAll(CODE_BLOCK_SELECTOR);
+const makeWebsiteRegexp = url => {
+  return new RegExp(
+    httpRegexp.source + wwwRegexp.source + escapeRegexp(url) + tailRegexp.source
+  );
+};
 
-// Process each code block to add a copy button
-codeBlocks.forEach(codeBlock => {
-  const firstChild = codeBlock.firstChild;
-  // There is already a copy button, so don't create another button
-  // For now just return
-  if (firstChild.tagName === 'BUTTON' && firstChild.dataset.clipboardText) {
-    console.log('not creating button');
+// CSS selector to match a code block on a page
+const patterns = [
+  {
+    pattern: makeWebsiteRegexp('github.com'),
+    codeBlockSelector: 'div.highlight',
+  },
+  {
+    pattern: makeWebsiteRegexp('stackoverflow.com'),
+  },
+];
+
+patterns.forEach(({ pattern, codeBlockSelector = 'pre' }) => {
+  if (!pattern.test(document.URL)) {
     return;
   }
+  // Get all code blocks
+  const codeBlocks = document.querySelectorAll(codeBlockSelector);
 
-  // Element that contains the code snippet we want to copy
-  // AFAIK, this is always the first (and only) child of the code block
-  const codeSnippetEl = firstChild;
+  // Process each code block to add a copy button
+  codeBlocks.forEach(codeBlock => {
+    const firstChild = codeBlock.firstChild;
+    // There is already a copy button, so don't create another button
+    // For now just return
+    if (firstChild.tagName === 'BUTTON' && firstChild.dataset.clipboardText) {
+      console.log('not creating button');
+      return;
+    }
 
-  // Extract the code snippet
-  const codeSnippet = codeSnippetEl.innerText;
+    // Element that contains the code snippet we want to copy
+    // AFAIK, this is always the first (and only) child of the code block
+    const codeSnippetEl = firstChild;
 
-  // Need to set `position: relative` for the code block
-  codeBlock.style.position = 'relative';
+    // Extract the code snippet
+    const codeSnippet = codeSnippetEl.innerText;
 
-  // Add a button to the code block
-  const copyButton = document.createElement('button');
-  copyButton.style.position = 'absolute';
-  copyButton.style.top = 0;
-  copyButton.style.right = 0;
-  copyButton.innerText = 'Copy';
-  copyButton.dataset.clipboardText = codeSnippet;
+    // Need to set `position: relative` for the code block
+    codeBlock.style.position = 'relative';
 
-  // Insert the copy button before the code snippet element
-  codeBlock.insertBefore(copyButton, codeSnippetEl);
-});
+    // Add a button to the code block
+    const copyButton = document.createElement('button');
+    copyButton.style.position = 'absolute';
+    copyButton.style.top = 0;
+    copyButton.style.right = 0;
+    copyButton.innerText = 'Copy';
+    copyButton.dataset.clipboardText = codeSnippet;
 
-// Instanstiate Clipboard.js instance
-const clip = new Clipboard('button[data-clipboard-text]');
+    // Insert the copy button before the code snippet element
+    codeBlock.insertBefore(copyButton, codeSnippetEl);
+  });
 
-clip.on('success', function(e) {
-  e.trigger.innerText = 'Copied!';
-  setTimeout(() => {
-    e.trigger.innerText = 'Copy';
-  }, 3000);
-});
+  // Instanstiate Clipboard.js instance
+  const clip = new Clipboard('button[data-clipboard-text]');
 
-clip.on('error', function(e) {
-  e.trigger.innerText = 'Failed :(';
-  setTimeout(() => {
-    e.trigger.innerText = 'Copy';
-  }, 3000);
+  clip.on('success', function(e) {
+    e.trigger.innerText = 'Copied!';
+    setTimeout(() => {
+      e.trigger.innerText = 'Copy';
+    }, 3000);
+  });
+
+  clip.on('error', function(e) {
+    e.trigger.innerText = 'Failed :(';
+    setTimeout(() => {
+      e.trigger.innerText = 'Copy';
+    }, 3000);
+  });
 });
