a7fb41206876acbc4456a6e1bee171fea947c450
JMH <jheld135@gmail.com>
2017-10-19 20:24:59 -0500

    Use Gulp to build content_scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
+const rollup = require('rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
 
-gulp.task('default', () => {
+gulp.task('default', () => {});
 
-})
+gulp.task('build:background', () => {
+  console.log('building background page');
+});
+
+gulp.task('build:content_scripts', async () => {
+  const bundle = await rollup.rollup({
+    input: './src/content_scripts/main.js',
+    plugins: [resolve(), commonjs()],
+  });
+
+  await bundle.write({
+    file: './dist/content_scripts/main.js',
+    format: 'iife',
+  });
+});
+
+gulp.task('build:all', ['build:background', 'build:content_scripts']);
