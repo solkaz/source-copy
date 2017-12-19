4098df5752f46841879d06d2ed91bdd53746a4aa
JMH <jheld135@gmail.com>
2017-12-18 19:47:15 -0600

    Disable button if name & pattern are empty; prettify

--- src/options_ui/new-option-form.js
+++ src/options_ui/new-option-form.js
@@ -1,37 +1,63 @@
 import m from 'mithril';
 
-export const Col = (el) => m('div.col', [el])
+export const validateForm = ({ name, pattern }) => Boolean(name && pattern);
+
+export const Col = el => m('div.col', [el]);
 
 export class NewOptionForm {
-	oninit(vnode) {
-		vnode.state.newOption = {
-			name: '',
-			pattern: '',
-			selector: '',
-			enabled: true
-		};
-	}
+  oninit(vnode) {
+    vnode.state.newOption = {
+      name: '',
+      pattern: '',
+      selector: '',
+      enabled: true,
+    };
+  }
 
-	view(vnode) {
-		const {
-			name,
-			pattern,
-			selector,
-			enabled
-		} = vnode.state.newOption;
+  view(vnode) {
+    const { name, pattern, selector, enabled } = vnode.state.newOption;
 
     return m('form', [
       m('div.row.p-3', [
-	      Col(m('input.form-control', { type: 'text', placeholder: 'Name', value: name})),
-	      Col(m('input.form-control', { type: 'text', placeholder: 'Pattern', value: pattern })),
-	      Col(m('input.form-control', { type: 'text', placeholder: 'Selector (Optional)', value: selector })),
-	      Col(m('div.form-check.form-check-inline', [
+        Col(
+          m('input.form-control', {
+            type: 'text',
+            placeholder: 'Name',
+            value: name,
+          })
+        ),
+        Col(
+          m('input.form-control', {
+            type: 'text',
+            placeholder: 'Pattern',
+            value: pattern,
+          })
+        ),
+        Col(
+          m('input.form-control', {
+            type: 'text',
+            placeholder: 'Selector (Optional)',
+            value: selector,
+          })
+        ),
+        Col(
+          m('div.form-check.form-check-inline', [
             m('label.form-check-label', [
-	            m('input.form-check-input', { type: 'checkbox', checked: enabled }),
+              m('input.form-check-input', {
+                type: 'checkbox',
+                checked: enabled,
+              }),
               'Enabled',
             ]),
-          ])),
-	      Col(m('button.btn.btn-primary', { type: 'button' }, '+')),
+          ])
+        ),
+        Col(
+          m(
+            'button.btn.btn-primary',
+            { type: 'button', disabled: !validateForm(vnode.state.newOption) },
+            '+'
+          )
+        ),
       ]),
     ]);
   }
