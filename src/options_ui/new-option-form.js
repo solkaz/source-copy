5ef6f9ea205e615ab072e5b1420101648764534a
JMH <jheld135@gmail.com>
2017-12-18 19:30:14 -0600

    Add state to NewOptionForm; add Col util function

--- src/options_ui/new-option-form.js
+++ src/options_ui/new-option-form.js
@@ -1,26 +1,37 @@
 import m from 'mithril';
 
+export const Col = (el) => m('div.col', [el])
+
 export class NewOptionForm {
-  view() {
+	oninit(vnode) {
+		vnode.state.newOption = {
+			name: '',
+			pattern: '',
+			selector: '',
+			enabled: true
+		};
+	}
+
+	view(vnode) {
+		const {
+			name,
+			pattern,
+			selector,
+			enabled
+		} = vnode.state.newOption;
+
     return m('form', [
       m('div.row.p-3', [
-        m('div.col', [
-          m('input.form-control', { type: 'text', placeholder: 'Name' }),
-        ]),
-        m('div.col', [
-          m('input.form-control', { type: 'text', placeholder: 'Pattern' }),
-        ]),
-        m('div.col', [
-          m('div.form-check.form-check-inline', [
+	      Col(m('input.form-control', { type: 'text', placeholder: 'Name', value: name})),
+	      Col(m('input.form-control', { type: 'text', placeholder: 'Pattern', value: pattern })),
+	      Col(m('input.form-control', { type: 'text', placeholder: 'Selector (Optional)', value: selector })),
+	      Col(m('div.form-check.form-check-inline', [
             m('label.form-check-label', [
-              m('input.form-check-input', { type: 'checkbox' }),
+	            m('input.form-check-input', { type: 'checkbox', checked: enabled }),
               'Enabled',
             ]),
-          ]),
-        ]),
-        m('div.col', [
-          m('input.form-control', { type: 'text', placeholder: 'Selector (Optional)' }),
-        ]),
+          ])),
+	      Col(m('button.btn.btn-primary', { type: 'button' }, '+')),
       ]),
     ]);
   }
